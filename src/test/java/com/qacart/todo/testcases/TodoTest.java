cba1539ef0e9196db38873c466b8f00cbd9afbb5
wajdi abdelhafidh <abdelhafidhwajdi@gmail.com>
2023-12-12 11:25:51 +0100

    NOn

--- src/test/java/com/qacart/todo/testcases/TodoTest.java
+++ src/test/java/com/qacart/todo/testcases/TodoTest.java
@@ -14,7 +14,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
-//
+
 public class TodoTest extends BaseTest {
     @Test(description = "should Be Able To Add Todo")
     public void shouldBeAbleToAddTodo() {
@@ -50,5 +50,5 @@ public class TodoTest extends BaseTest {
         Assert.assertTrue(isNotTodosDisplayed);
 
         }
-
+/////
 }
